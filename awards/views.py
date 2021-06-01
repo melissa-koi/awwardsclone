0395278d40d3811453d4be81746bdbc9368d42a7
melissa-koi <melissawangui3@gmail.com>
2021-06-01 05:48:05 +0300

    change views

--- awards/views.py
+++ awards/views.py
@@ -29,16 +29,24 @@ def site(request, pk):
             rate.user = user
             rate.website = site
             rate.save()
+    else:
+        form = RateForm()
+
 
         if rates is not None:
             design = request.POST.get("design")
             usability = request.POST.get("usability")
             content = request.POST.get("content")
             creativity = request.POST.get("creativity")
-            rating = Rate(design=design,usability=usability,content=content,creativity=creativity,user=request.user)
+            rating = Rate(design=design,usability=usability,content=content,creativity=creativity,user=request.user, website=site)
             rating.save()
-    else:
-        form = RateForm()
+        else:
+            design = request.POST.get("design")
+            usability = request.POST.get("usability")
+            content = request.POST.get("content")
+            creativity = request.POST.get("creativity")
+            rating = Rate(design=design,usability=usability,content=content,creativity=creativity,user=request.user, website=site)
+            rating.save()
 
 
     return render(request, 'site.html', {"title": title, "photo": photo, "form":form, "rates":rates, "site":site})
