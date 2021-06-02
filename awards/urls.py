1125f513ec1043b1149e1e0dc7c9c28ca8c8f01b
melissa-koi <melissawangui3@gmail.com>
2021-06-02 12:11:15 +0300

    heroku4

--- awards/urls.py
+++ awards/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', login_required(views.home), name = 'home'),
+    path('', views.home, name = 'home'),
     path('register/', views.registerUser, name = 'register'),
     path('login/', views.loginUser, name = 'login'),
     path('logout/', views.logoutUser, name = 'logout'),
-    path('site/<str:pk>/', login_required(views.site), name = 'site'),
-    path('profile/<str:username>/', login_required(views.profile) , name = 'profile'),
-    path('post/', login_required(views.post_website) , name = 'post_website'),
-    path('profile/update/<int:profile_id>/', login_required(views.update_profile) , name='update_profile'),
-    path('search/', login_required(views.search_results), name = 'search_results'),
+    path('site/<str:pk>/', views.site, name = 'site'),
+    path('profile/<str:username>/', views.profile, name = 'profile'),
+    path('post/', views.post_website, name = 'post_website'),
+    path('profile/update/<int:profile_id>/', views.update_profile, name='update_profile'),
+    path('search/', views.search_results, name = 'search_results'),
 ]
